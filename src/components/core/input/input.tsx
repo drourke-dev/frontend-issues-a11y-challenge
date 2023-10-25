bf7e36e8ba2c13ad7aa41525c3c36cdc7376e0a0
Dáire O' Rourke <daire.o@deliveryhero.com>
2023-10-25 14:27:47 +0200

    Form elements do not have associated labels.

--- src/components/core/input/input.tsx
+++ src/components/core/input/input.tsx
@@ -4,16 +4,30 @@ type InputElementProps = ComponentPropsWithoutRef<"input">;
 
 interface InputOwnProps {
   label: string;
+  id: string;
 }
 
 export type InputProps = InputOwnProps &
   Omit<InputElementProps, keyof InputOwnProps>;
 
-export const Input = ({ className, label, type }: InputProps) => {
+export const Input = ({
+  id,
+  className,
+  label,
+  type,
+  placeholder,
+}: InputProps) => {
   return (
     <div className={`c-input ${className ? className : ""}`.trim()}>
-      <label className="c-input__label">{label}</label>
-      <input type={type} className="c-input__input" />
+      <label htmlFor={id} className="c-input__label">
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        className="c-input__input"
+        placeholder={placeholder}
+      />
     </div>
   );
 };
