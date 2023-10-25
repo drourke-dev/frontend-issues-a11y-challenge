7b4e441e595f5f851f141e4632a92c3d52636c81
Dáire O' Rourke <daire.o@deliveryhero.com>
2023-10-25 14:33:14 +0200

    Select elements do not have associated label elements.

--- src/components/core/select/select.tsx
+++ src/components/core/select/select.tsx
@@ -4,16 +4,21 @@ type SelectElementProps = ComponentPropsWithoutRef<"select">;
 
 interface SelectOwnProps {
   label: string;
+  id: string;
 }
 
 export type SelectProps = SelectOwnProps &
   Omit<SelectElementProps, keyof SelectOwnProps>;
 
-export const Select = ({ className, label, children }: SelectProps) => {
+export const Select = ({ id, className, label, children }: SelectProps) => {
   return (
     <div className={`c-select ${className ? className : ""}`.trim()}>
-      <label className="c-Select__label">{label}</label>
-      <select className="c-select__select">{children}</select>
+      <label htmlFor={id} className="c-Select__label">
+        {label}
+      </label>
+      <select id={id} className="c-select__select">
+        {children}
+      </select>
     </div>
   );
 };
